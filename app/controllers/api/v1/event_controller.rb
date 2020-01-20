4547bc93412a0e6c2145da776ad12bd72dca6942
AndresFMoya <andresfmoya@gmail.com>
2020-01-20 10:52:18 -0500

    chore: add linter files

--- app/controllers/api/v1/event_controller.rb
+++ app/controllers/api/v1/event_controller.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class Api::V1::EventController < ApplicationController
   def index
-    render json: {events: ['Kansas City Chiefs - San Francisco 49ers']}
+    render json: { events: ['Kansas City Chiefs - San Francisco 49ers'] }
   end
 end
