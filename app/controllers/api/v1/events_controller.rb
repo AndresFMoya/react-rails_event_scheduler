eb160672f9f0702b1ee877e223987aa8eac2a20a
AndresFMoya <andresfmoya@gmail.com>
2020-01-20 19:23:53 -0500

    fix: change Events controler name

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::EventController < ApplicationController
+class Api::V1::EventsController < ApplicationController
   def index
     render json: { events: ['Kansas City Chiefs - San Francisco 49ers'] }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'events', to: 'event#index'
+      get 'events', to: 'events#index'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
