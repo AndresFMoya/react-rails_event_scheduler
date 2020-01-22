93cfd38a9d73aa8bf5ab6ba6b11c3aa74eb1924f
AndresFMoya <andresfmoya@gmail.com>
2020-01-22 18:35:26 -0500

    chore: import Redux store and add Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,10 @@
-import React, { useState, useEffect, Component } from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { loadEvents } from '../redux/actions/actionCreators';
+import React from 'react';
+import EventsContainer from './components/EventsContainer';
 
+const App = () => (
+  <div className="app container panel-bg">
+    <EventsContainer />
+  </div>
+);
 
-class App extends Component {
-  
-  const {events} = props
-  
-  componentDidMount() {
-    this.fetchEvents();
-  }
-
-  fetchEvents() {
-    axios.get('/api/v1/events')
-      .then(response => {
-        this.props.dispatch(loadEvents(response.data));
-      })
-      .catch(error => console.log(error));
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <EventsList todos={props.events} />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({
-  events: state.events,
-});
-
-export default connect(mapStateToProps, null)(App);
+export default App;
