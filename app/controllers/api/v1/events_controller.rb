eeb6f68ea9ed16b3632b8633d392fe75167a8455
AndresFMoya <andresfmoya@gmail.com>
2020-01-23 10:43:07 -0500

    chore: fix linter errors

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,4 +3,27 @@ class Api::V1::EventsController < ApplicationController
     events = Event.all
     render json: events
   end
+
+  def create
+    event = Event.create(event_params)
+    render json: event
+  end
+
+  def update
+    event = Event.find(params[:id])
+    event.update_attributes(event_params)
+    render json: event
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    head :no_content, status: :ok
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date_start, :date_end, :status, :city)
+  end
 end
