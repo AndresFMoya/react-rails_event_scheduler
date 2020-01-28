d8ee087b88f064cbb166eada52ee305f96f1b548
AndresFMoya <andresfmoya@gmail.com>
2020-01-28 12:03:34 -0500

    feat: add style to event description page

--- client/src/components/EventDescription.js
+++ client/src/components/EventDescription.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+import './EventDescription.scss';
 
 const EventDescription = (props) => {
   const initialEventState = {
@@ -23,15 +24,48 @@ const EventDescription = (props) => {
   };
 
   return (
-    <div className="card" key={event.id} id={event.id}>
-      <button type="button" onClick={goBack}>Go Back</button>
-      <div className="event-info p-5">
-        <div className="event">{ event.title }</div>
-        <div className="event">{ event.description }</div>
-        <div className="event">{ event.city }</div>
-        <div className="event">{ event.country }</div>
-        <div className="event">{ event.location }</div>
-        <div className="event">{ event.status }</div>
+    <div className="card event-description" key={event.id} id={event.id}>
+      <div className="top-event-description">
+        <div className="d-flex">
+          <i className="fas fa-arrow-left back-icon" onClick={goBack} />
+          <div className="event-description-title">{ event.title }</div>
+        </div>
+        <div className="card top-middle-event-description bg-white m-3 mt-4 p-4 ">
+          <div className="event-description-date-card border-bottom d-flex flex-row pb-3">
+            <span><i className="far fa-calendar-alt icon"/></span>
+            <div className="ml-3">
+              <div className="date-label">Date & Time</div>
+              <div className="date w-100">{ event.date_start }</div>
+            </div>
+          </div>
+          <div className="event-description-date-card d-flex flex-row pt-3">
+            <span><i className="fas fa-university icon"/></span>
+            <div className="w-100 ml-3">
+              <div className="date-label">City</div>
+              <div className="city w-100">{ event.city }</div>
+            </div>
+          </div>
+        </div>
+        <div className="card m-3 justify-content-between align-items-center d-flex flex-row add-to-schedule">
+          <span className="m-2 add-label">Add to your Schedule </span>
+          <i className="fas fa-plus m-2 icon" />
+        </div>
+
+      </div>
+
+      <div className="card p-3 info">
+        <span className="infoLabel">Description </span>
+        <div className="description">{ event.description }</div>
+      </div>
+  
+      <div className="card p-3 location">
+        <span className="infoLabel">Location </span>
+        <div className="description">{ event.location }</div>
+      </div>
+  
+      <div className="card p-3 status">
+        <span className="infoLabel">Status </span>
+        <div className="description">{ event.status }</div>
       </div>
     </div>
   );
