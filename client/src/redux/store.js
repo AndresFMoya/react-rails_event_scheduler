1e21fde5bd8a960ca9cc0233696c38898fedf84f
AndresFMoya <andresfmoya@gmail.com>
2020-02-04 15:39:17 -0500

    feat: add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,27 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers/rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function configureStore() {
-  return createStore(
-    rootReducer, composeEnhancers(
-      applyMiddleware(thunk),
-    ),
-  );
-}
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(
+  persistedReducer, composeEnhancers(
+    applyMiddleware(thunk),
+  ),
+);
+
+const persistor = persistStore(store);
+
+export {
+  store,
+  persistor,
+};
