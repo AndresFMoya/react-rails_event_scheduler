fce0fb0399a8ffa83837a71528700a2e288cbb5b
AndresFMoya <andresfmoya@gmail.com>
2020-02-04 11:18:34 -0500

    feat: add checkFollowed function

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -8,7 +8,7 @@ import './Event.scss';
 
 const Event = (props) => {
   const {
-    event, user, isAuthenticated,
+    event, user, isAuthenticated, followed_events,
   } = props;
 
   const history = useHistory();
@@ -23,6 +23,20 @@ const Event = (props) => {
       setState({
         isFollowed: true,
       });
+    }
+  };
+
+  const checkFollowed = () => {
+    if ((props.followed_events.filter(el => el.event_id === props.event.id)).length > 0) {
+      return (
+        <button type="button" className="event-button-remove" onClick={handleRemove}><span className="button-add w-100">+ Your Schedule </span>
+        </button>
+      )
+    } else {
+      return (
+        <button type="button" className="event-button-add" onClick={handleSubmit}><span className="button-add w-100">+ Your Schedule</span>
+        </button>
+      )
     }
   };
 
@@ -47,10 +61,7 @@ const Event = (props) => {
           <div className="event-title">{ event.title }</div>
           <div className="event-city pb-2">{ event.city }</div>
         </div>
-        { isAuthenticated
-          ? ((isAuthenticated && user.event_follower_ids.includes(event.id))
-        || (state.isFollowed === true)) ? <button type="button" className="event-button-remove" onClick={handleRemove}><span className="button-add w-100">+ Your Schedule </span></button>
-            : <button type="button" className="event-button-add" onClick={handleSubmit}><span className="button-add w-100">+ Your Schedule</span></button> : <div /> }
+        { isAuthenticated ? checkFollowed() : <div />}
       </div>
     </li>
   );
@@ -67,6 +78,7 @@ Event.propTypes = {
 const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
   user: state.auth.currentUser,
+  followed_events: state.followed_events,
 });
 
 export default connect(mapStateToProps, { createEventFollower, deleteEventFollower })(Event);
